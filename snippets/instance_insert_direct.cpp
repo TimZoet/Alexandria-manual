fc9a8b461ea4371e17d4797fe9f7dead58030bd2
Tim Zoet <ts.zoet@gmail.com>
2021-05-31 21:05:40 +0200

    Wrote part of the ObjectHandler section

---
+++ snippets/instance_insert_direct.cpp
@@ -0,0 +1,5 @@
+// Insert a new instance.
+Foo inst;
+inst.prop = ...;
+H->insert(inst);
+// Identifier is now valid.
---
+++ snippets/instance_update_direct.cpp
@@ -0,0 +1,9 @@
+// Create instance.
+Foo inst;
+H->create(&inst);
+
+// Modify.
+inst.prop = ...;
+
+// Update database.
+H->update(inst);
